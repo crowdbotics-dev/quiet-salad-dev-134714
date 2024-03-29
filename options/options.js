ece40f1d9833b394fefbd489773e2a635245da3b
Crowdbotics <engineering@crowdbotics.com>
2024-03-29 17:00:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiet-salad-dev-134714",
+  url: "https://quiet-salad-dev-134714.botics.co",
+  api: "https://quiet-salad-dev-134714.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
